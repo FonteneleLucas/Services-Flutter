f8b16e3faa0fc8f880c500edad99076e567790ee
FonteneleLucas <lucasfind@hotmail.com>
2019-09-14 14:06:15 -0300

    home page

---
+++ lib/homePage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => new _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+//      resizeToAvoidBottomPadding: false,
+      body: Center(
+        child: Text("Hello world"),
+      )
+    );
+  }
+
+}
